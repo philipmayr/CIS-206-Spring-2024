6b2e53d7282f8a57d3b0d8e1b8b1f798cc8550f9
Phil Samuel <philipmayr@outlook.com>
2024-01-28 18:34:56 -0600

    Update Problem 1.py

--- Assignment II/Problem 1.py
+++ Assignment II/Problem 1.py
@@ -2,6 +2,7 @@
 
 # Problem 1
 print("Enter the lengths of the 3 sides of a triangle below. The program will tell whether the triangle is right, acute, or obtuse.")
+print()
 
 side1 = float(input("Enter side 1 length: "))
 side2 = float(input("Enter side 2 length: "))
@@ -10,6 +11,8 @@ side3 = float(input("Enter side 3 length: "))
 side1sq = side1 ** 2
 side2sq = side2 ** 2
 side3sq = side3 ** 2
+
+print()
 
 if side1sq + side2sq == side3sq:
     print("The triangle is right.")
