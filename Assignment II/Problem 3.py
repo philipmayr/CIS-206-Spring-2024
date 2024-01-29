a6b388a798509d434b201c78fd1bc4f254943a40
Phil Samuel <philipmayr@outlook.com>
2024-01-28 18:43:24 -0600

    Update Problem 3.py

--- Assignment II/Problem 3.py
+++ Assignment II/Problem 3.py
@@ -1,7 +1,7 @@
 # ASSIGNMENT II - CIS 206 - phil may'r
 
 # Problem 3
-print("This program finds the best option for author remuneration to choose from three publisher's options.")
+print("This program finds the best option to choose for author remuneration from three publisher's options.")
 print()
 # first option consts
 DELIVERY_OF_FINAL_MANUSCRIPT_REMUNERATION = 5000
