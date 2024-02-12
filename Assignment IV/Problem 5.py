3422d0957443ec2ff1b716129aa4378cee6c45bd
Phil Samuel <philipmayr@outlook.com>
2024-02-12 11:09:05 -0600

    Create Problem 5.py

---
+++ Assignment IV/Problem 5.py
@@ -0,0 +1,18 @@
+# ASSIGNMENT IV - CIS 206 - phil may'r
+
+# Problem 4
+
+y_n = input("Do you wish to run to the program? Enter yes/no: ")
+
+while (y_n.lower() == 'yes'):
+    first_exam_score = int(input("Enter first exam score: "))
+    if first_exam_score < 0 or first_exam_score > 100:
+        y_n = input("Entry outside valid range. Run program again? Enter yes/no: ")
+        continue
+    second_exam_score = int(input("Enter second exam score: "))
+    if second_exam_score < 0 or second_exam_score > 100:
+        y_n = input("Entry outside valid range. Run program again? Enter yes/no: ")
+        continue
+    total_points = (first_exam_score * 0.6) + (second_exam_score * 0.4)
+    print("Total points: " + str(total_points))
+    break
