ccf8ab4c1d9455e9a04f1502a647d13de98936e1
Phil Samuel <philipmayr@outlook.com>
2024-02-12 10:17:43 -0600

    Create Problem 1.py

---
+++ Assignment IV/Problem 1.py
@@ -0,0 +1,14 @@
+# ASSIGNMENT IV - CIS 206 - phil may'r
+
+# Problem 1
+
+while (True):
+    num = int(input("Enter an integer from 10 to 20, bounds  inclusive: "))
+    if (num < 10 or num > 20):
+        print("Integer outside accepted range.")
+        pass
+    else:
+        break
+    
+for i in range(num + 1):
+    print(i)
