000fd3e7bc5b870eaa0b8e2e544d906df2e71618
Phil Samuel <philipmayr@outlook.com>
2024-02-12 10:50:45 -0600

    Create Problem 4.py

---
+++ Assignment IV/Problem 4.py
@@ -0,0 +1,13 @@
+# ASSIGNMENT IV - CIS 206 - phil may'r
+
+# Problem 4
+
+fav_names = ["Benjamin", "Caleb", "Daniel", "David", "Jacob", "James", "Jonathan", "Joshua", "Matthew", "Paul"]
+
+for name in fav_names:
+    print(name)
+    
+print("··········")
+
+for i in range(0, len(fav_names)):
+    print(fav_names[i])
