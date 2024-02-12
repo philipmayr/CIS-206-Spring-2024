4748c89e47c2a5729c22ff94aab8ab5bbb310493
Phil Samuel <philipmayr@outlook.com>
2024-02-12 10:19:33 -0600

    Update Problem 1.py

--- Assignment IV/Problem 1.py
+++ Assignment IV/Problem 1.py
@@ -3,7 +3,7 @@
 # Problem 1
 
 while (True):
-    num = int(input("Enter an integer from 10 to 20, bounds  inclusive: "))
+    num = int(input("Enter an integer from 10 to 20, bounds inclusive: "))
     if (num < 10 or num > 20):
         print("Integer outside accepted range.")
         pass
