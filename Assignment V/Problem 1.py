e56cad928b8fbbf7c53906b67d58b3b9b5e362d7
Phil Samuel <philipmayr@outlook.com>
2024-02-23 10:17:14 -0600

    Create Problem 1.py

---
+++ Assignment V/Problem 1.py
@@ -0,0 +1,7 @@
+# ASSIGNMENT V - CIS 206 - phil may'r
+
+# Problem 1
+
+full_name = input("Enter first and last name: ")
+formatted_full_name = full_name.title()
+print("You entered: " + formatted_full_name)
