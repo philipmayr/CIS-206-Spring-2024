5835a1254c686e800f4f0b8ed1c568d0292c18ee
Phil Samuel <philipmayr@outlook.com>
2024-02-23 12:25:06 -0600

    Update Problem 4.py

--- Assignment V/Problem 4.py
+++ Assignment V/Problem 4.py
@@ -5,9 +5,17 @@
 def find_vowels(string):
     vowels = {'a', 'e', 'i', 'o', 'u'}
     vowel_count = 0
-    for char in string:
-        for vowel in vowels:
-            if char.lower() == vowel:
+    for index, char in enumerate(string):
+        if char.lower() in vowels:
+            vowel_count += 1
+        if char.lower() == 'y':
+            if index == len(string) - 1:
+                vowel_count += 1
+            elif not (string[index + 1].isalpha()):
+                vowel_count += 1
+            elif not (index == 0) and 
+                 not (string[index - 1] in vowels) and 
+                 not (string[index + 1] in vowels):
                 vowel_count += 1
                 
     return vowel_count
@@ -16,9 +24,8 @@ def find_consonants(string):
     vowels = {'a', 'e', 'i', 'o', 'u'}
     consonant_count = 0
     for char in string:
-        for vowel in vowels:
-            if char.lower != vowel and char.isalpha():
-                consonant_count += 1
+        if char.lower() not in vowels and char.isalpha():
+            consonant_count += 1
                 
     return consonant_count
                 
@@ -27,4 +34,5 @@ string = input("Please enter a string of characters: ")
 vowel_count = find_vowels(string)
 consonant_count = find_consonants(string)
 
-print("The input string contains " + str(vowel_count) + " vowels and " + str(consonant_count) + " consonants.")
+print("The input string contains " + str(vowel_count) + " vowels and " +
+                                     str(consonant_count) + " consonants.")
