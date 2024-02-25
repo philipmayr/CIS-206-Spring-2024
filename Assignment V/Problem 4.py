f1f13ea60f560e9df8bdd9bc75b57319de80c91c
Phil Samuel <philipmayr@outlook.com>
2024-02-25 08:50:44 -0600

    Update Problem 4.py

--- Assignment V/Problem 4.py
+++ Assignment V/Problem 4.py
@@ -2,6 +2,8 @@
 
 # Problem 4
 
+# Reference: https://www.woodwardenglish.com/letter-y-vowel-or-consonant/
+
 def find_vowels(string):
     vowels = {'a', 'e', 'i', 'o', 'u'}
     vowel_count = 0
@@ -13,8 +15,8 @@ def find_vowels(string):
                 vowel_count += 1
             elif not (string[index + 1].isalpha()):
                 vowel_count += 1
-            elif not (index == 0) and 
-                 not (string[index - 1] in vowels) and 
+            elif not (index == 0) and \
+                 not (string[index - 1] in vowels) and \
                  not (string[index + 1] in vowels):
                 vowel_count += 1
                 
@@ -23,16 +25,29 @@ def find_vowels(string):
 def find_consonants(string):
     vowels = {'a', 'e', 'i', 'o', 'u'}
     consonant_count = 0
-    for char in string:
+    for index, char in enumerate(string):
         if char.lower() not in vowels and char.isalpha():
-            consonant_count += 1
+            if char.lower() == "y":
+                if index == 0:
+                    consonant_count += 1
+                    continue
+                elif not (index == len(string) - 1):
+                    if string[index - 1] in vowels or \
+                       string[index + 1] in vowels:
+                        consonant_count += 1
+                        continue
+            if char.lower() != "y":
+                consonant_count += 1
                 
     return consonant_count
-                
-string = input("Please enter a string of characters: ")
-
-vowel_count = find_vowels(string)
-consonant_count = find_consonants(string)
-
-print("The input string contains " + str(vowel_count) + " vowels and " +
-                                     str(consonant_count) + " consonants.")
+    
+# Inf. loop for testing
+while(True):       
+    string = input("Please enter a string of characters: ")
+    
+    vowel_count = find_vowels(string)
+    consonant_count = find_consonants(string)
+    
+    print("'" + string + "' contains " + str(vowel_count) + " vowel(s) and " +
+                                         str(consonant_count) + " consonant(s).",
+                                         end="\n\n")
