6767000c0aeaef70a6560ab8ec14543b347b5fd8
Phil Samuel <philipmayr@outlook.com>
2024-03-01 17:16:43 -0600

    Update Session 7 Assignment.py

--- Assignment VII/Session 7 Assignment.py
+++ Assignment VII/Session 7 Assignment.py
@@ -13,28 +13,33 @@ def format_email_address(last_name, first_name):
     
     return address
 
-with open('EmpData.csv', 'r') as csv_file:
-    reader = csv.DictReader(csv_file)
+try:
+    with open('EmpData.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
 
-    with open('NewEmpData.csv', 'w', newline='') as new_csv_file:
-        fieldnames = ["Last_Name", "First_Name", "Gross_Pay", "EMail"]
+        with open('NewEmpData.csv', 'w', newline='') as new_csv_file:
+            fieldnames = ["Last_Name", "First_Name", "Gross_Pay", "EMail"]
 
-        writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+            writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames)
+            writer.writeheader()
 
-        for line in reader:
-            last_name = line['Last_Name']
-            first_name = line['First_Name']
-            hours = float(line['Hours'])
-            rate = float(line['Rate'])
+            for line in reader:
+                last_name = line['Last_Name']
+                first_name = line['First_Name']
+                hours = float(line['Hours'])
+                rate = float(line['Rate'])
 
-            gross_pay = compute_gross_pay(hours, rate)
-            email_address = format_email_address(last_name, first_name)
+                gross_pay = compute_gross_pay(hours, rate)
+                email_address = format_email_address(last_name, first_name)
 
-            name = {"Last_Name": last_name, 
-                    "First_Name": first_name, 
-                    "Gross_Pay": gross_pay, 
-                    "EMail": email_address}
+                name = {"Last_Name": last_name, 
+                        "First_Name": first_name, 
+                        "Gross_Pay": gross_pay, 
+                        "EMail": email_address}
 
-            writer.writerow(name)
+                writer.writerow(name)
+except IOError:
+    print("Read/Write Error")
+except:
+    print("Unknown Error")
             
