6c828c546e8072edd3dd3dd028075ed04b221199
Phil Samuel <philipmayr@outlook.com>
2024-03-11 15:38:22 -0500

    Update Solution 5.py

--- Assignment VIII/Solution 5.py
+++ Assignment VIII/Solution 5.py
@@ -1,4 +1,6 @@
-state_capitals = {
+import random
+
+STATE_CAPITALS = {
     "Alabama": "Montgomery",
     "Alaska": "Juneau",
     "Arizona": "Phoenix",
@@ -50,3 +52,19 @@ state_capitals = {
     "Wisconsin": "Madison",
     "Wyoming": "Cheyenne",
 }
+
+correct_response_counter = 0
+incorrect_response_counter = 0
+
+while correct_response_counter < 50:
+    random_state = random.choice(list(STATE_CAPITALS))
+    print("What is the capital of " + random_state + "?")
+    input_capital = input("Enter the state's capital: ")
+    
+    if (STATE_CAPITALS[random_state].lower() == input_capital.lower().strip()):
+        correct_response_counter += 1
+        print("Correct!" + "(" + str(correct_response_counter) + ")")
+    else:
+        incorrect_response_counter += 1
+        print("Incorrect!" + "(" + str(incorrect_response_counter) + ")")
+    print()
