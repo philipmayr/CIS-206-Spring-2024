555acec2fc232e427f6d71bb6a4e47b168a722a9
Phil Samuel <philipmayr@outlook.com>
2024-03-11 18:01:59 -0500

    Update Solution 3.py

--- Assignment VIII/Solution 3.py
+++ Assignment VIII/Solution 3.py
@@ -1,28 +1,40 @@
+# ASSIGNMENT VIII - CIS 206 - phil may'r
+
+# Problem 3
+
+import re
+
 def get_midpoint(first_endpoint, second_endpoint):
-    x1, y1 = first_endpoint
-    x2, y2 = second_endpoint
-    x = (x1 + x2) / 2 
-    y = (y1 + y2) / 2
+    first_x, first_y = first_endpoint
+    second_x, second_y = second_endpoint
     
-    midpoint = x, y
+    midpoint_x = (first_x + second_x) / 2.0 
+    midpoint_y = (first_y + second_y) / 2.0
+    
+    midpoint = midpoint_x, midpoint_y
     
     return midpoint
     
-print("This program finds the midpoint of two endpoints.\n")
-first_endpoint_input = input("Enter ordered pair (x1, y1) of first endpoint: ")
-second_endpoint_input = input("Enter ordered pair (x2, y2) of second endpoint: ")
+print("This program will find the midpoint of two endpoints.\n")
 
-x1 = x2 = y1 = y2 = 0
+first_endpoint = input("Enter ordered pair (x1, y1) of first endpoint: ")
+second_endpoint = input("Enter ordered pair (x2, y2) of second endpoint: ")
 
-for i in first_endpoint_input:
-    numbers = []
-    x, y = numbers
-    
-for i in second_endpoint_input:
-    numbers = []
-    x, y = numbers
+first_x = second_x = first_y = second_y = 0
 
-first_endpoint = x1, y1
-second_endpoint = x2, y2
+for i in first_endpoint:
+    numbers = re.findall('[-]?\d+', first_endpoint)
+    first_x, first_y = numbers
+        
+for i in second_endpoint:
+    numbers = re.findall('[-]?\d+', second_endpoint)
+    second_x, second_y = numbers
+
+first_endpoint = int(first_x), int(first_y)
+second_endpoint = int(second_x), int(second_y)
 
 midpoint = get_midpoint(first_endpoint, second_endpoint)
+
+print("\n" + str(midpoint) + " is the midpoint of points " + 
+             str(first_endpoint) + " and " + 
+             str(second_endpoint) + ".")
