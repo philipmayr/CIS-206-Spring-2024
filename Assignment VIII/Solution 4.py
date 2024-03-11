d1102046fba3cc4e36f1a87a64cd89b532d60f3d
Phil Samuel <philipmayr@outlook.com>
2024-03-11 14:35:40 -0500

    Create Solution 4.py

---
+++ Assignment VIII/Solution 4.py
@@ -0,0 +1,10 @@
+# ASSIGNMENT VIII - CIS 206 - phil may'r
+
+# Solution 4
+
+set1 = {'a', 'b', 'c', '1', '2', '3'}
+set2 = {'d', 'e', 'f', '4', '5', '6'}
+
+set3 = set1.union(set2)
+
+print(sorted(set3))
