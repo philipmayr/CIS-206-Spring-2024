6dd69024310643ac092047921af02e2249e4dcc2
Phil Samuel <philipmayr@outlook.com>
2024-03-11 18:44:04 -0500

    Update Solution 3.py

--- Assignment VIII/Solution 3.py
+++ Assignment VIII/Solution 3.py
@@ -3,23 +3,24 @@
 # Solution 3
 
 # This program is an example of using, packing, and unpacking tuples.
-# Currently works with integer coordinate values only.
 
 # Reference: 
-# https://stackoverflow.com/a/12643073
+# https://stackoverflow.com/a/4703409
 
 import re
 
 def find_midpoint(first_endpoint, second_endpoint):
+    # unpack tuples
     first_x, first_y = first_endpoint
     second_x, second_y = second_endpoint
 
-    # casting to int so as to drop '.0'
-    midpoint_x = int((first_x + second_x) / 2)
-    midpoint_y = int((first_y + second_y) / 2)
-    
+    midpoint_x = (first_x + second_x) / 2
+    midpoint_y = (first_y + second_y) / 2
+
+    # pack tuple
     midpoint = midpoint_x, midpoint_y
-    
+
+    # return tuple
     return midpoint
     
 print("This program will find the midpoint of two endpoints.\n")
@@ -30,20 +31,21 @@ second_endpoint = input("Enter ordered pair (x2, y2) of second endpoint: ")
 first_x = second_x = first_y = second_y = 0
 
 for i in first_endpoint:
-    # finds integers
-    numbers = re.findall('[-]?\d+', first_endpoint)
+    numbers = re.findall("[-]?(?:\d*\.*\d+)", first_endpoint)
     first_x, first_y = numbers
         
 for i in second_endpoint:
-    # finds integers
-    numbers = re.findall('[-]?\d+', second_endpoint)
+    numbers = re.findall("[-]?(?:\d*\.*\d+)", second_endpoint)
     second_x, second_y = numbers
 
-first_endpoint = int(first_x), int(first_y)
-second_endpoint = int(second_x), int(second_y)
+# pack tuples
+first_endpoint = float(first_x), float(first_y)
+second_endpoint = float(second_x), float(second_y)
 
+# call function, passing in tuple arguments thereto, initialize variable with function call's tuple return value
 midpoint = find_midpoint(first_endpoint, second_endpoint)
 
+# print string representations of tuples
 print("\n" + str(midpoint) + " is the midpoint of points " + 
              str(first_endpoint) + " and " + 
              str(second_endpoint) + ".")
