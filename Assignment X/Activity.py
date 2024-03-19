23a4c5fb11b9f01cd10667898835a72ff977b078
Phil Samuel <philipmayr@outlook.com>
2024-03-19 08:26:05 -0500

    Update Activity.py

--- Assignment X/Activity.py
+++ Assignment X/Activity.py
@@ -1,7 +1,6 @@
+import json
 import datetime
-import urllib.request
-import urllib.parse
-import re
+from urllib.request import urlopen
 
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
@@ -10,10 +9,13 @@ current_year = current_date.year
 current_month = current_date.month
 previous_month = current_month - 1
 
-print(current_date)
-print(current_year)
-print(current_month)
-
-url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access"
+url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/"
 
 # top_1000_most_visited_articles
+
+url = url + str(current_year) + '/' + str(previous_month).rjust(2, '0') + "/all-days"
+
+with urlopen(url) as url:
+    data = json.load(url)
+    top_articles = data["items"][0]["articles"]
+    print(top_articles)
