63ae405a62f345f74cb486f3ee953ce3c97fc4f1
Phil Samuel <philipmayr@outlook.com>
2024-04-28 14:34:47 -0500

    Update Activity.py

--- Assignment XI/Activity.py
+++ Assignment XI/Activity.py
@@ -1,22 +1,70 @@
 import sqlite3
 
-connection = sqlite3.connect('colors.db')
-cursor = connection.cursor()
-
 def create_table():
-	cursor.execute('CREATE TABLE IF NOT EXISTS colors(color TEXT, red REAL, green REAL, blue REAL, alpha REAL)')
+	cursor.execute("CREATE TABLE IF NOT EXISTS colors(color TEXT, red REAL, green REAL, blue REAL, alpha REAL)")
 
 def feed_data():
-	cursor.execute("INSERT INTO colors VALUES('Blue', 50.3, 75.6, 113.3, 1.0)")
-	cursor.execute("INSERT INTO colors VALUES('Purple', 75.6, 50.4, 113.3, 1.0)")
-	cursor.execute("INSERT INTO colors VALUES('Bronze', 113.3, 75.6, 50.4, 1.0)")
-	cursor.execute("INSERT INTO colors VALUES('Amaranth', 113.3, 50.4, 75.6, 1.0)")
-	cursor.execute("INSERT INTO colors VALUES('Chlorophyll', 50.4, 113.3, 75.6, 1.0)")
-	cursor.execute("INSERT INTO colors VALUES('Olive', 75.6, 113.3, 50.4, 1.0)")
+	data = [('Blue', 50.3, 75.6, 113.3, 1.0),
+		('Purple', 75.6, 50.4, 113.3, 1.0),
+		('Bronze', 113.3, 75.6, 50.4, 1.0),
+		('Amaranth', 113.3, 50.4, 75.6, 1.0),
+		('Chlorophyll', 50.4, 113.3, 75.6, 1.0),
+		('Olive', 75.6, 113.3, 50.4, 1.0)]
+			
+	cursor.executemany("INSERT INTO colors VALUES (?, ?, ?, ?, ?)", data)
+	
+def list_rows_and_columns():
+	cursor.execute("SELECT * FROM colors")
+	for row in cursor.fetchall():
+		print(row)
+		
+def print_row(row):
+	cursor.execute("SELECT * FROM colors WHERE color=" + row)
+	for row in cursor.fetchall():
+		print(row)
+		
+def list_column(column):
+	cursor.execute("SELECT " + column + " FROM colors")
+	for row in cursor.fetchall():
+		print(row[0])
+		
+def sum_column(column):
+	cursor.execute("SELECT SUM(" + column + ") FROM colors")
+	print(cursor.fetchone()[0])
+	
+def count_rows():
+	cursor.execute("SELECT * FROM colors")
+	count = len(cursor.fetchall())
+	print(count)
 
-	connection.commit()
-	cursor.close()
-	connection.close()
+def show_rows_and_columns():
+	cursor.execute("SELECT * FROM colors")
+	print(cursor.fetchall())
+	
+
+connection = sqlite3.connect("colors.db")
+cursor = connection.cursor()
 
 create_table()
 feed_data()
+
+list_rows_and_columns()
+print()
+print_row("'Blue'")
+print()
+list_column("color")
+print()
+sum_column("red")
+print()
+sum_column("green")
+print()
+sum_column("blue")
+print()
+count_rows()
+print()
+show_rows_and_columns()
+print()
+
+connection.commit()
+cursor.close()
+connection.close()
