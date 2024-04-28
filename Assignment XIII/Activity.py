3b68e2236d948f72eb483f2ea1d691a213baf89c
Phil Samuel <philipmayr@outlook.com>
2024-04-28 10:04:44 -0500

    Update Activity.py

--- Assignment XIII/Activity.py
+++ Assignment XIII/Activity.py
@@ -1,17 +1,30 @@
 class Employee:
     
-    def __init__(self, first_name, last_name, job_level, annual_salary):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.job_level = job_level
-        self.annual_salary = annual_salary
-        if job_level == 1:
-            self.short_term_bonus = annual_salary * 0.25
-        elif job_level == 2:
-            self.short_term_bonus = annual_salary * 0.20
-        elif job_level == 3:
-            self.short_term_bonus = annual_salary * 0.10
-        self.long_term_bonus = annual_salary * 0.10
+    def __init__(self, first_name=None, last_name=None, job_level=None, annual_salary=None):
+        self.first_name = None
+        self.last_name = None
+        self.job_level = None
+        self.annual_salary = None
+        self.short_term_bonus = None
+        self.long_term_bonus = None
+        
+        if first_name is not None:
+            self.first_name = first_name
+        if last_name is not None:
+            self.last_name = last_name
+        if job_level is not None:
+            self.job_level = job_level
+        if annual_salary is not None:
+            self.annual_salary = annual_salary
+        if job_level is not None and annual_salary is not None:
+            if job_level == 1:
+                self.short_term_bonus = annual_salary * 0.25
+            elif job_level == 2:
+                self.short_term_bonus = annual_salary * 0.20
+            elif job_level == 3:
+                self.short_term_bonus = annual_salary * 0.10
+        if annual_salary is not None:
+            self.long_term_bonus = annual_salary * 0.10
         
 new_employee = Employee("Sam", "Bowman", 1, 70000)
 
