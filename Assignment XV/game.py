42050933f7bae288eaebe021fe13d11daba6fbbc
Phil Samuel <philipmayr@outlook.com>
2024-05-13 10:19:42 -0500

    Update game.py

--- Assignment XV/game.py
+++ Assignment XV/game.py
@@ -1,3 +1,4 @@
+
 # Reference: Tic Tac Toe Game - Python Tkinter GUI Tutorial #113
 #            https://www.youtube.com/watch?v=xx0qmpuA-vM
 
@@ -5,6 +6,7 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
+root.resizable(False, False)
 root.title("Noughts & Crosses")
 
 x = '𝕏'
@@ -14,6 +16,38 @@ olive = "#4B7132"
 white = "#ffffff"
 clicked = True
 count = 0
+
+def reset_game():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+
+    clicked = True
+    count = 0
+
+    b1 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
+
+    b4 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
+
+    b7 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
+
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
+
 
 def disable_buttons():
     b1.config(state=DISABLED)
@@ -145,6 +179,10 @@ def check_game_state():
         messagebox.showinfo("Noughts & Crosses", "O Wins!")
         disable_buttons()
 
+    if count == 9 and winner == False:
+        messagebox.showinfo("Noughts & Crosses", "Game Tied!")
+        disable_buttons()
+
  
 def b_click(b):
     global clicked, count
@@ -162,29 +200,12 @@ def b_click(b):
         messagebox.showerror("Noughts & Crosses", "That square is already taken!\nPick another one!")
 
 
-b1 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
-b2 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
-b3 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
+game_menu = Menu(root)
+root.config(menu=game_menu)
 
-b4 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
-b5 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
-b6 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
+options_menu = Menu(game_menu, tearoff=False)
+game_menu.add_cascade(label="Game", menu=options_menu)
+options_menu.add_command(label="Reset Game", command=reset_game)
 
-b7 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
-b8 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
-b9 = Button(root, text=" ", font=("Helvetica", 24), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
-
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
-
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
-
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
-
-
+reset_game()
 root.mainloop()
